f9cbd6081408a459ac85f242237bc7c682396087
WhiteCrow <11510511@mail.sustc.edu.cn>
2018-10-12 14:20:31 +0800

    projecting

---
+++ prj1/code_check_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+from code_check import CodeCheck
+
+def main():
+    #code_checker = CodeCheck("Your code address", 15)
+    code_checker = CodeCheck("/home/hf/CS303-ArtificialIntelligence/prj1/go.py", 15)
+    if not code_checker.check_code():
+        print(code_checker.errormsg)
+    else:
+        print('pass')
+
+if __name__ == '__main__':
+    main()
+    # print('test')
