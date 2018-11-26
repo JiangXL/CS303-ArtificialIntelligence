5aa5b1404d914dc738b7c24990070dc961e99bea
WhiteCrow <11510511@mail.sustc.edu.cn>
2018-11-26 22:49:25 +0800

    writing ISE

--- prj3/ISE.py
+++ prj3/ISE.py
@@ -4,12 +4,32 @@ Description: Return value of the estimated influence spread with given graph
              and seeds
 Input : -i <social network> -s <seed set> -m <diffusion model> -t <time budget> 
 Output: the value of the estimated influence spread
+
 | Version | Commit
 |   0.1   |   
+
 '''
 import sys
 import os
 import argparse
+import numpy
+
+time_budget = 0
+graph
+
+def LT(graph, seeds):
+   actived = []
+   threshold = {}
+   for node in graph:
+       threshold[node] = numpy.random() 
+
+   saturation = 0
+   
+   while(not saturation ): #Todo: add time limit later
+       for seed in seeds:
+           for neighbour in graph:
+               
+   return len(actived)
 
 if __name__ == '__main__':
    parser = argparse.ArgumentParser()
@@ -21,5 +41,7 @@ if __name__ == '__main__':
    input_file = args.input_file
    seed_file = args.seed_file
    model = args.model
-   time_budget = args.time
+   global time_budget = args.time
    print(input_file, seed_file, model, time_budget)
+
+   
